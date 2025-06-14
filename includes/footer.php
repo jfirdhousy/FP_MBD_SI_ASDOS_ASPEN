fe71c7898451ad20f8e0c604d40a28628be47dab
daffadrh <150494438+daffadrh@users.noreply.github.com>
2025-06-14 15:03:06 +0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,13 @@
+</main> <footer class="bg-dark text-white py-3 mt-auto"> <div class="row m-0 align-items-center"> <div class="col-sm-6 text-sm-start text-center"> <p class="mb-0">&copy; <?php echo date("Y"); ?> Sistem Informasi Lowongan Asisten Dosen/Penelitian.</p>
+            </div>
+            <div class="col-sm-6 text-sm-end text-center"> <p class="mb-0">Dibuat oleh Kelompok 4.</p>
+            </div>
+        </div>
+        </footer>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcSanxVvXn0zXKLdJ6uYd" crossorigin="anonymous"></script>
+
+    <script src="<?php echo BASE_URL; ?>js/script.js"></script>
+
+</body>
+</html>
