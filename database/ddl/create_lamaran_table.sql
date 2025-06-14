778b20093cab083dee0c2892dab226be450cc697
daffadrh <150494438+daffadrh@users.noreply.github.com>
2025-06-15 05:45:20 +0700

    Update create_lamaran_table.sql

--- database/ddl/create_lamaran_table.sql
+++ database/ddl/create_lamaran_table.sql
@@ -1,16 +1,26 @@
--- database/ddl/create_mahasiswa_table.sql
+-- database/ddl/create_lamaran_table.sql
 
-CREATE TABLE mahasiswa (
-    nrp VARCHAR(10) PRIMARY KEY NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(50) NOT NULL,
-    nama_mahasiswa VARCHAR(255) NOT NULL,
-    no_telp VARCHAR(16),
+CREATE TABLE lamaran (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tanggal_melamar TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status_lamaran ENUM('Pending', 'Diterima', 'Ditolak', 'Ditinjau') NOT NULL DEFAULT 'Pending',
+    cv_url VARCHAR(1024),
+    transkrip_url VARCHAR(1024),
+    note_dosen TEXT,
+    lowongan_id INT NOT NULL,
+    mahasiswa_nrp VARCHAR(10) NOT NULL,
 
-    FOREIGN KEY (departemen_id) REFERENCES departemen(id)
+    FOREIGN KEY (lowongan_id) REFERENCES lowongan(id)
         ON UPDATE CASCADE
-        ON DELETE SET NULL
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (mahasiswa_nrp) REFERENCES mahasiswa(nrp)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+
+    UNIQUE (lowongan_id, mahasiswa_nrp)
 );
 
-CREATE INDEX idx_mahasiswa_email ON mahasiswa(email);
-CREATE INDEX idx_mahasiswa_departemen_id ON mahasiswa(departemen_id);
+CREATE INDEX idx_lamaran_lowongan_id ON lamaran(lowongan_id);
+CREATE INDEX idx_lamaran_mahasiswa_nrp ON lamaran(mahasiswa_nrp);
+CREATE INDEX idx_lamaran_status ON lamaran(status_lamaran);
