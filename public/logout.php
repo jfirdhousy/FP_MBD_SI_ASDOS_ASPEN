9300a6c47dad0027c05dcb2b31103347775fe89a
daffadrh <150494438+daffadrh@users.noreply.github.com>
2025-06-14 15:04:17 +0700

    Add files via upload

---
+++ public/logout.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+session_destroy();
+
+header("Location: /sistem_lowongan_asisten_simple/public/login.php");
+exit();
+?>
