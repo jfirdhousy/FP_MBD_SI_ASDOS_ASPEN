1f9e5817eeba926a9c5299431ef32f0962a5ef51
Siti Zahra Ananda Kurniawan <162027699+szahraak@users.noreply.github.com>
2025-06-15 19:02:54 +0700

    Update create_dosen_table.sql

--- database/ddl/create_dosen_table.sql
+++ database/ddl/create_dosen_table.sql
@@ -1,14 +1,16 @@
 -- DDL for table 'dosen'
 CREATE TABLE dosen (
-    nip VARCHAR(18) PRIMARY KEY,
+    nip VARCHAR(18) PRIMARY KEY NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(50) NOT NULL,
     nama_dosen VARCHAR(255),
     no_telp VARCHAR(16),
     departemen_id INT,
-    CONSTRAINT fk_dosen_departemen
-        FOREIGN KEY (departemen_id)
-        REFERENCES departemen(id)
+    
+    FOREIGN KEY (departemen_id) REFERENCES departemen(id)
         ON UPDATE CASCADE
-        ON DELETE RESTRICT
+        ON DELETE SET NULL
 );
+
+CREATE INDEX idx_dosen_email ON dosen(email);
+CREATE INDEX idx_dosen_departemen_id ON dosen(departemen_id);
