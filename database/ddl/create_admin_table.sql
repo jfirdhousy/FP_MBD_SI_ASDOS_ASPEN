64ecf61eacf8e1a05026765b39e969a544e896c6
Siti Zahra Ananda Kurniawan <162027699+szahraak@users.noreply.github.com>
2025-06-15 19:03:35 +0700

    Update create_admin_table.sql

--- database/ddl/create_admin_table.sql
+++ database/ddl/create_admin_table.sql
@@ -1,7 +1,9 @@
 -- DDL for table 'admin'
 CREATE TABLE admin (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(50) NOT NULL,
     nama_admin VARCHAR(255)
 );
+
+CREATE UNIQUE INDEX idx_admin_email ON admin(email);
