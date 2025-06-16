f7a38953e59f20d84ba0cf73ae985fdc578c94c4
daffadrh <150494438+daffadrh@users.noreply.github.com>
2025-06-16 11:28:38 +0700

    Update get_name_by_nrp.sql

--- database/queries/functions_procedures/get_name_by_nrp.sql
+++ database/queries/functions_procedures/get_name_by_nrp.sql
@@ -1,1 +1,12 @@
-
+-- Function 1: Mengambil nama mahasiswa berdasarkan NRP
+DELIMITER $$
+CREATE FUNCTION GET_MAHASISWA_NAMA(p_nrp VARCHAR(10))
+RETURNS VARCHAR(255)
+READS SQL DATA
+BEGIN
+    DECLARE nama_mhs VARCHAR(255);
+    SELECT nama_mahasiswa INTO nama_mhs FROM mahasiswa WHERE nrp = p_nrp;
+    RETURN nama_mhs;
+END$$
+DELIMITER ;
+    
