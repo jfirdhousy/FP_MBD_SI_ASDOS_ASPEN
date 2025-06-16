cb0827b62cb98fb1ce48f1d62bb99673bf167b0a
Nadya Pricha Aufa <103725682+ndpricha@users.noreply.github.com>
2025-06-16 10:17:30 +0700

    Create create_mahasiswa_skill.sql

---
+++ database/ddl/create_mahasiswa_skill.sql
@@ -0,0 +1,11 @@
+--ddl
+
+CREATE TABLE mahasiswa_skill (
+    mahasiswa_n VARCHAR(10) NOT NULL,
+    skill_id INT NOT NULL,
+    PRIMARY KEY (mahasiswa_n, skill_id),
+    FOREIGN KEY (mahasiswa_n) REFERENCES mahasiswa(nrp) ON DELETE CASCADE,
+    FOREIGN KEY (skill_id) REFERENCES skill(id) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_skill_id ON mahasiswa_skill(skill_id);
