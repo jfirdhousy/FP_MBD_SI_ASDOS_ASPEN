92ec217b4a68138aec8058d74ca1604d71ac178d
Siti Zahra Ananda Kurniawan <162027699+szahraak@users.noreply.github.com>
2025-06-16 22:24:51 +0700

    Update view_active_lowongan.sql

--- database/queries/view/view_active_lowongan.sql
+++ database/queries/view/view_active_lowongan.sql
@@ -1,10 +1,12 @@
 CREATE VIEW VIEW_LOWONGAN_ACTIVE AS
 SELECT
+    l.id AS lowongan_id,
     l.nama_lowongan,
     l.jenis AS jenis_lowongan,
     l.jumlah_diterima,
     l.deadline,
     l.tanggal_post,
+    d.nip AS dosen_nip,
     d.nama_dosen,
     dep.nama_departemen AS departemen
 FROM
