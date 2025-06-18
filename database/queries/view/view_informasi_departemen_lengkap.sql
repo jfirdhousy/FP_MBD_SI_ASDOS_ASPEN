28709656335b3e2eaed77f2a2c839d4325964f2f
doci <144414218+jfirdhousy@users.noreply.github.com>
2025-06-18 19:57:18 +0700

    Update view_informasi_departemen_lengkap.sql

--- database/queries/view/view_informasi_departemen_lengkap.sql
+++ database/queries/view/view_informasi_departemen_lengkap.sql
@@ -1,16 +1,13 @@
+-- Ini adalah contoh hipotetis jika VIEW_INFORMASI_DEPARTEMEN_LENGKAP memanggil fungsi:
 CREATE VIEW VIEW_INFORMASI_DEPARTEMEN_LENGKAP AS
 SELECT
     d.id AS departemen_id,
     d.nama_departemen,
     COUNT(DISTINCT dos.nip) AS jumlah_dosen,
-    COUNT(DISTINCT mhs.nrp) AS jumlah_mahasiswa
+    FUNC_GET_JUMLAH_MAHASISWA_DEPARTEMEN(d.id) AS jumlah_mahasiswa -- Panggilan fungsi di sini
 FROM
     departemen d
 LEFT JOIN
     dosen dos ON d.id = dos.departemen_id
-LEFT JOIN
-    mahasiswa mhs ON d.id = mhs.departemen_id
 GROUP BY
-    d.id, d.nama_departemen
-ORDER BY
-    d.nama_departemen;
+    d.id, d.nama_departemen;
